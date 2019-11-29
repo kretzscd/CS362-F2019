73ee20b92acd166d6b24512935287c4a434a29f0
Huffmajo <huffmajo@oregonstate.edu>
2019-11-29 12:50:32 -0800

    Cleaned up unittest1.c

--- projects/FinalProject-Bugs/dominion/unittest1.c
+++ projects/FinalProject-Bugs/dominion/unittest1.c
@@ -30,7 +30,7 @@ int main()
 	card = mine;
 
 	// start testing
-	printf("************************\n* Mine Unit Test Start *\n************************\n");
+	printf("************************\n* Bug 1 Unit Test Start *\n************************\n");
 
 	printf("\nTEST 1: Trash a copper to gain a silver\n");
 	memcpy(&test, &base, sizeof(struct gameState)); // refresh test state
@@ -68,88 +68,6 @@ int main()
 	}
 	testAssert(1, inDiscard);
 
-/*
-	printf("\nTEST 2: Trash a silver to get a duchy\n");
-	memcpy(&test, &base, sizeof(struct gameState)); // refresh test state
-	choice1 = 2; // hand position of silver to trash
-	choice2 = duchy; // card to get in exchange
-	handPos = 0; // hand position of mine
-	returnState = mineEffect(&test, choice1, choice2, handPos, player); // play card
-
-	// should return -1
-	printf("Can't trade for non-treasure card. Should return -1\n");
-	testAssert(-1, returnState);
-
-	// hand unchanged
-	printf("Hand size unchanged\n");
-	testAssert(base.handCount[player], test.handCount[player]);
-
-	// discard should remain unchanged
-	printf("Discard count unchanged\n");
-	testAssert(base.discardCount[player], test.discardCount[player]);
-
-	// actions unchanged
-	printf("Number of actions unchanged\n");
-	testAssert(base.numActions, test.numActions);
-
-	// coins remain the same
-	printf("Coins are unchanged\n");
-	testAssert(base.coins, test.coins);
-
-	printf("\nTEST 3: Trash estate for a copper\n");
-	memcpy(&test, &base, sizeof(struct gameState)); // refresh test state
-	choice1 = 4; // hand position of estate to trash
-	choice2 = copper; // card to get in exchange
-	handPos = 0; // hand position of mine
-	returnState = mineEffect(&test, choice1, choice2, handPos, player); // play card
-
-	// should return -1
-	printf("Can't trade non-treasure. Should return -1\n");
-	testAssert(-1, returnState);
-
-	// hand unchanged
-	printf("Hand size unchanged\n");
-	testAssert(base.handCount[player], test.handCount[player]);
-
-	// discard should remain unchanged
-	printf("Discard count unchanged\n");
-	testAssert(base.discardCount[player], test.discardCount[player]);
-
-	// actions unchanged
-	printf("Number of actions unchanged\n");
-	testAssert(base.numActions, test.numActions);
-
-	// coins remain the same
-	printf("Coins are unchanged\n");
-	testAssert(base.coins, test.coins);
-
-	printf("\nTEST 4: Trash a copper for gain a gold\n");
-	memcpy(&test, &base, sizeof(struct gameState)); // refresh test state
-	choice1 = 1; // hand position of copper to trash
-	choice2 = gold; // card to get in exchange
-	handPos = 0; // hand position of mine
-	returnState = mineEffect(&test, choice1, choice2, handPos, player); // play card
-
-	// should return -1
-	printf("Gold too expensive for copper. should return -1\n");
-	testAssert(-1, returnState);
-
-	// hand unchanged
-	printf("Hand size unchanged\n");
-	testAssert(base.handCount[player], test.handCount[player]);
-
-	// discard should remain unchanged
-	printf("Discard count unchanged\n");
-	testAssert(base.discardCount[player], test.discardCount[player]);
-
-	// actions unchanged
-	printf("Number of actions unchanged\n");
-	testAssert(base.numActions, test.numActions);
-
-	// coins remain the same
-	printf("Coins are unchanged\n");
-	testAssert(base.coins, test.coins);
-*/
 	printf("Unit Test Complete\n");
 
 	return 0;
